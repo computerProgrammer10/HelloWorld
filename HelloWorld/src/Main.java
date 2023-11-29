fbcf2e34ece41dbd2898b7237e4cec1455af19a7
computerProgrammer10 <152317359+computerProgrammer10@users.noreply.github.com>
2023-11-29 17:24:52 +0000

    something happened
    its crazy

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -8,5 +8,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("I love computer science!");
         System.out.println("Hi!");
+        System.out.println("I am on replit!");
     }
 }
